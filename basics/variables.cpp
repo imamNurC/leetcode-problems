419c3f848198fac8f4ddbf7b81b36a6569d0ab78
imamNurC <imam>
2024-09-01 15:08:21 +0700

    pre proccess, compile

---
+++ basics/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+// initial value 
+
+int main(int argc, char const *argv[])
+{
+    
+int a=0;
+int b(0);
+
+    return 0;
+}
---
+++ compiling/compile.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+// namespace std;
+
+int main( /*int argc, char const *argv[] */)
+{
+    std:: cout << "Tesss Stringg!!" << std::endl;    
+    // int a = 0;
+    // int b = 0;
+    return 0;
+
+
+}
+// #include "head.h"
+
+
+/**
+cpp ->  pre-proccess -> 
+
+Common command to compile 
+
+g++ <file>.cpp || ./<compiled_code>.out
+
+
+#PRE-PROCCESSING
+g++ -E <file_target>.cpp  > <output_compile>.p
+
+
+#COMPILE
+g++ -c <file_target>.cpp
+#show deassemble
+objdump -d <file_target>.cpp
+
+
+
+ * **/
